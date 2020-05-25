9697f1eb3f6340f0db4d75e982d06994d5c40d91
Maurizio Rinder <moonrider1510@gmail.com>
2020-05-25 23:45:56 +0200

    recreate project to start with a react single page application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div><p>Hello World!</p></div>
+        )
+    }  
+}
+
+export default App;
